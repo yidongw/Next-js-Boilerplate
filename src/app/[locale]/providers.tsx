1513852ef429cd6d51f9450ad37112693044aee6
Alan Wang <wy.dong96@gmail.com>
2025-03-16 11:35:08 +0800

    feat: change default theme to dark

--- src/app/[locale]/providers.tsx
+++ src/app/[locale]/providers.tsx
@@ -24,7 +24,7 @@ export function Providers({ children }: Readonly<{ children: ReactNode }>) {
         <RainbowKitProvider coolMode appInfo={appInfo}>
           <ThemeProvider
             attribute="class"
-            defaultTheme="system"
+            defaultTheme="dark"
             enableSystem
             disableTransitionOnChange
           >
